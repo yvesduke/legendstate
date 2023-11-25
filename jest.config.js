686e69783e6198b464d73b3f79cd1058cc018778
jmquilario <jmquilario@gmail.com>
2023-11-25 18:40:10 +0000

    test: added built-in jest coverage instead of istanbul

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   preset: 'react-native',
-  // moduleDirectories: ['node_modules', path.join(__dirname, 'src')], // uncomment this if using absolute imports
+  displayName: {name: 'LEGEND STATE', color: 'blue'},
+  coverageReporters: [
+    'clover',
+    'json',
+    'lcov',
+    'text-summary',
+    ['text', {skipFull: true}],
+  ],
+  coverageDirectory: 'report',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
