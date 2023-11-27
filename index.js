174c186c65debf064e0bed62f2ae5aa0f32cca79
jmquilario <jmquilario@gmail.com>
2023-11-27 18:34:16 +0000

    ci: adding test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import {Sentry} from '@sentry/react-native';
+import * as Sentry from '@sentry/react-native';
 // import * as Sentry from '@sentry/react-native';
 
 // Sentry.init({
