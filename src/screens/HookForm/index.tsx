34ff2304be3fe1f0dfd2961dc4a1deb94a3d5348
Santhiyavelu <santhiyav1507@gmail.com>
2023-11-28 11:30:22 +0000

    Hook-form implementation

--- src/screens/HookForm/index.tsx
+++ src/screens/HookForm/index.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import {Text} from 'tamagui';
+import {View, TextInput, Button, Alert} from 'react-native';
+import {useForm, SubmitHandler} from 'react-hook-form';
+import styles from './styles';
 
-const HookForm = () => {
-  return <Text>HookForm</Text>;
-};
+enum GenderEnum {
+  female = 'female',
+  male = 'male',
+  other = 'other',
+}
 
-export default HookForm;
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: GenderEnum;
+}
+
+export default function HookForm() {
+  const {register, handleSubmit} = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data, 'submit');
+    Alert.alert(JSON.stringify(data));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>First Name</Text>
+      <TextInput style={styles.input} {...register('firstName')} />
+      <Text style={styles.label}>Last Name</Text>
+      <TextInput style={styles.input} {...register('lastName')} />
+      <Text style={styles.label}>Age</Text>
+      <TextInput style={styles.input} {...register('age')} />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+}
---
+++ src/screens/HookForm/styles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+});
+
+export default styles;
