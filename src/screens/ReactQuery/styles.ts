fede9520c319492f7431524c06ee5885e733a248
Yves <yvesduke06@live.co.uk>
2023-12-19 13:27:44 +0000

    ReactQuery Example

---
+++ src/screens/ReactQuery/styles.ts
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+
+  tittle: {
+    height: 45,
+    margin: 12,
+    borderWidth: 6,
+    padding: 10,
+    backgroundColor: '#f5f5f5',
+    marginHorizontal: 15,
+    borderColor: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 21,
+    color: '#696969',
+    fontFamily: 'Apple SD Gothic Neo',
+    textAlign: 'center',
+  },
+
+  nameText: {
+    fontSize: 18,
+    // fontWeight: 'bold',
+    borderWidth: 6,
+    padding: 18,
+    backgroundColor: '#f5f5f5',
+    borderColor: '#ffffff',
+    marginHorizontal: 9,
+    color: '#696969',
+    fontFamily: 'Apple SD Gothic Neo',
+  },
+});
